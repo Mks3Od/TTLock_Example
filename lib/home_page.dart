1cf9ae637b637a77389885e753c381ce7039bbef
Manish <manish@dbsindia.in>
2026-04-03 14:44:37 +0530

    Initial fix error commit

---
+++ lib/home_page.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:bmprogresshud/progresshud.dart';
+import 'package:ttlock_flutter/ttelectricMeter.dart';
+import 'package:ttlock_flutter_example/lock_page.dart';
+import 'scan_page.dart';
+import 'config.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage() : super();
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  BuildContext? _context;
+
+  void _startScanGateway() {
+    if (GatewayConfig.uid == 0 ||
+        GatewayConfig.ttlockLoginPassword.length == 0) {
+      String text = 'Please config the ttlockUid and the ttlockLoginPassword';
+      ProgressHud.of(_context!)!.showAndDismiss(ProgressHudType.error, text);
+
+      return;
+    }
+    _startScan(ScanType.gateway);
+  }
+
+  void _startScanLock() {
+    _startScan(ScanType.lock);
+  }
+
+  void _startScanElectricMeter() {
+    ElectricMeterServerParamMode electricMeterServerParamMode =
+        ElectricMeterServerParamMode();
+    // electricMeterServerParamMode.url = 'https://cntestservlet.sciener.cn/lock/electricMeterCommand/executeCommand';
+    // electricMeterServerParamMode.clientId = '607ab4bcc9504a5da58c43575a1b3746';
+    // electricMeterServerParamMode.accessToken = 'VgC8yDPW/jr6V31nNAcCEkFLNA6o27cQ6OZDjF4iNbKbSz1kU5LcoMh0I4xgbZNZ';
+    electricMeterServerParamMode.url = "https://cntestservlet.sciener.cn";
+    electricMeterServerParamMode.clientId = '607ab4bcc9504a5da58c43575a1b3746';
+    electricMeterServerParamMode.accessToken =
+        'fj81Mf4Mnglw5knoaTmjLG8c4H2fdhpWB37wwFJh2dI=';
+
+    TTElectricMeter.configServer(electricMeterServerParamMode);
+    _startScan(ScanType.electricMeter);
+  }
+
+  void _startKeyPadPage() {
+    _startScan(ScanType.keyPad);
+  }
+
+  void _startScan(ScanType scanType) {
+    Navigator.push(
+      context,
+      new MaterialPageRoute(
+        builder: (BuildContext context) {
+          return ScanPage(scanType: scanType);
+        },
+      ),
+    );
+  }
+
+  Widget getChild() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton(
+          child: Text('Lock', style: TextStyle(fontWeight: FontWeight.w600)),
+          onPressed: _startScanLock,
+        ),
+
+        ElevatedButton(
+          child: Text(
+            'Lock Navigate',
+            style: TextStyle(fontWeight: FontWeight.w600),
+          ),
+          onPressed: () {
+            String lockData =
+                "3eQp7X0qKkWIbdGflk1+Dkh+zTpe4QvQtY0NS3m7Jprs1r8m8aOfM6XaCKygNQM/P+RugZ+TiCT3VqOZPMlqm2dBIVSVSfpMm0x1IQ5gR4smQeBX0vTNgBniCwLOHdh9o7Va5uvwjfZBC68hdRiW9PMYqtQwljliVoH1H4RQKcC+ecdz9aHzq13Ko2EcabJkaY5758QLFX0OmLKwjNLyNCRJ41EFe6gxOijSXgeDLk1an02KQ7RIyuIkFabTdYfaQjsBMEwu1AaKKIZZStOvAuMUX3MshaD+GK8LppoOfIgIJvrcp9WQT5iRfsdNevu5eKoe05AVSpZVhT++EfFlQZQrNURBmGX";
+            String lockMac = "19:78:E7:B1:14:62";
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => LockPage(
+                  title: "title",
+                  lockData: lockData,
+                  lockMac: lockMac,
+                ),
+              ),
+            );
+          },
+        ),
+
+        // ElevatedButton(
+        //   child: Text('Gateway', style: TextStyle(fontWeight: FontWeight.w600)),
+        //   onPressed: _startScanGateway,
+        // ),
+        // ElevatedButton(
+        //   child: Text(
+        //     'Electric Meter',
+        //     style: TextStyle(fontWeight: FontWeight.w600),
+        //   ),
+        //   onPressed: _startScanElectricMeter,
+        // ),
+        // ElevatedButton(
+        //   child: Text('Key Pad', style: TextStyle(fontWeight: FontWeight.w600)),
+        //   onPressed: _startKeyPadPage,
+        // ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('TTLock Demo')),
+      body: Material(
+        child: ProgressHud(
+          child: Center(
+            child: Builder(
+              builder: (context) {
+                _context = context;
+                return getChild();
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
